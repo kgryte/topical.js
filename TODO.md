3f87987148ed3321fd9025759d619027729456d9
kgryte <kgryte@gmail.com>
2014-10-21 03:21:19 -0700

    [UPDATE] lib. WIP.

--- TODO.md
+++ TODO.md
@@ -1,2 +1,5 @@
 TODO
 ====
+
+1. Topic options
+2. Tests
--- test/test.js
+++ test/test.js
@@ -19,8 +19,8 @@ var expect = chai.expect,
 describe( 'topical', function tests() {
 	'use strict';
 
-	it( 'should export a function', function test() {
-		expect( topical ).to.be.a( 'function' );
+	it( 'should export an object', function test() {
+		expect( topical ).to.be.an( 'object' );
 	});
 
 	it( 'should do something' );
